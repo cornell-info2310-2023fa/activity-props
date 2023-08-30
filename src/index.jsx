63fed9563c5fd804de308f0b55623a3b213c808e
Kyle J. Harms <kyle.harms@cornell.edu>
2023-08-30 17:16:53 -0400

    router for page components

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
 import './index.css';
-import Header from './Header';
-import DateRefresh from './DateRefresh';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header title="Dynamic Website">
-      <p>This is a <strong>client-side rendered</strong> website.</p>
-    </Header>
-
-    <DateRefresh color="deepskyblue" />
-    <DateRefresh color="mediumorchid" actionText="Refresh Date"/>
-    <DateRefresh color="lightsalmon" actionText="Change Date"/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Outlet />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Header from '../components/Header';
+import DateRefresh from '../components/DateRefresh';
+
+export default function Home() {
+  return (
+    <>
+      <Header title="Home" />
+
+      <DateRefresh color="deepskyblue" />
+    <DateRefresh color="mediumorchid" actionText="Refresh Date"/>
+    <DateRefresh color="lightsalmon" actionText="Change Date"/>
+    </>
+  );
+}
